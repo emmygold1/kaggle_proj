0727d684efe259fdb4a3c5b2ae039ea3ecc87ee1
Fei Zhan <fei.zhan@jbadigital.com>
2016-01-16 09:59:24 +1100

    kaggle telstra: feature extraction copied from random forest to utils to be used for other models

--- telstra/utils.py
+++ telstra/utils.py
@@ -25,3 +25,49 @@ def get_evaluate(x, y, estimator, random_state=4332):
     evaluate_train = evaluate(estimator.predict_proba(x_train), y_train)
     evaluate_valid = evaluate(estimator.predict_proba(x_valid), y_valid)
     return evaluate_train, evaluate_valid
+
+train_event_start = 3
+test_event_start = train_event_start - 1
+train_feature_start = 56
+train_event_end = train_feature_start
+test_feature_start = train_feature_start - 1
+test_event_end = test_feature_start
+train_resource_start = 442
+train_feature_end = train_resource_start
+test_resource_start = train_resource_start - 1
+test_feature_end = test_resource_start
+train_severity_start = 452
+train_resource_end = train_severity_start
+test_severity_start = train_severity_start - 1
+test_resource_end = test_severity_start
+train_volume_start = 457
+train_severity_end = train_volume_start
+test_volume_start = train_volume_start - 1
+test_severity_end = test_volume_start
+train_end = 843
+test_end = train_end - 1
+
+ranges = [
+    ('event', range(train_event_start, train_feature_start)),
+    ('feature', range(train_feature_start, train_resource_start)), 
+    ('resource', range(train_resource_start, train_severity_start)),
+    ('severity', range(train_severity_start, train_volume_start)),
+    ('volume', range(train_volume_start, train_end))]
+
+def powerset(l):
+    if not l: return [[]]
+    return p(l[1:]) + [[l[0]] + x for x in p(l[1:])]
+
+def list_sum(args):
+    res = []
+    for arg in args:
+        res += arg
+    return res
+
+def string_sum(args):
+    res = ''
+    for arg in args:
+        res += arg + ' '
+    return res
+
+full_ranges = powerset(ranges)
